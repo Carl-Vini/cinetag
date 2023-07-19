d5b5c299d4db298c97dc81aa517351981f5ecd33
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-07-19 15:43:22 -0300

    criando componente container

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,17 +1,24 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Titulo from "components/Titulo";
+import videos from "json/db.json"
+import styles from "./Inicio.module.css"
 
-function Inicio () {
-    return ( 
+function Inicio() {
+    return (
         <>
-        
-    <Cabecalho />
-    <Banner imagem="home"/>
-    <Titulo>
-        <h1>Um lugar para guardar seus filmes e vídeos !</h1>
-    </Titulo>
-    </>
+            <Cabecalho />
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus filmes e vídeos !</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
     )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
+import Container from "components/Container";
 import Rodape from "components/Rodape";
+import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
@@ -6,9 +8,12 @@ const { BrowserRouter, Routes, Route } = require("react-router-dom");
 function AppRoutes() {
     return(
         <BrowserRouter>
+        <Container >
         <Routes>
             <Route path="/" element={<Inicio />} />
+            <Route path="/Favoritos" element={<Favoritos /> }/>
         </Routes>
+        </Container>
         <Rodape />
         </BrowserRouter>
     )
