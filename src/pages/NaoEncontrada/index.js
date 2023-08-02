94b2568c723160e60a22cb7b6f4f1eeb9f800bd9
vitor Higor <carlosviniciussampaio@hotmail.com>
2023-08-01 22:08:25 -0300

    tornando as rotas aninhadas

--- src/pages/NaoEncontrada/index.js
+++ src/pages/NaoEncontrada/index.js
@@ -3,7 +3,7 @@ import styles from "./naoEncontrada.module.css"
 function NaoEncontrada() {
     return (
         <section className={styles.container}>
-            <h2>Ops! Página não Encontrada</h2>
+            <h2>Ops! Página não Encontrada. </h2>
         </section>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
-import Cabecalho from "components/Cabecalho";
-import Container from "components/Container";
-import Rodape from "components/Rodape";
-import FavoritosProvider from "contextos/Favoritos";
+
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 import Player from "pages/Player";
 
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
@@ -12,18 +10,17 @@ const { BrowserRouter, Routes, Route } = require("react-router-dom");
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container >
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />} />
-                        <Route path="/Favoritos" element={<Favoritos />} />
-                        <Route path="/:id" element={<Player />}/>
-                        <Route path="*" element={<NaoEncontrada />} />
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />} />
+                    <Route path="Favoritos" element={<Favoritos />} />
+                    <Route path=":id" element={<Player />} />
+                    <Route path="*" element={<NaoEncontrada />} />
+                </Route>
+            </Routes>
+
+
         </BrowserRouter>
     )
 }
